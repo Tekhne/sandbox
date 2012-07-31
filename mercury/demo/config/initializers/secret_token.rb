aeebd1319d154675b5ddb65088879c4972aee887
Peter Havens <Peter.Havens@pinnacol.com>
2012-07-31 09:52:54 -0600

    Initial commit of mercury demo project.

---
+++ mercury/demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '3362bf592fc7290fe16f654483b89458e257ff24ef8f0ed02a00b2d3ade612a276ac07acf295c732b0779d0004014b3c4a9c8de2d792c000f759e980d91d322d'
