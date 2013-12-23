ca61f88d87d70b619a35f97e3eb0cf4a1ef3ab29
Peter Havens <Peter.Havens@pinnacol.com>
2013-12-23 15:06:57 -0700

    Start a new project for working on www.4clojure.com.

---
+++ clojure/www.4clojure.com/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to www.4clojure.com
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ clojure/www.4clojure.com/test/www/4clojure/com_test.clj
@@ -0,0 +1,7 @@
+(ns www.4clojure.com-test
+  (:require [clojure.test :refer :all]
+            [www.4clojure.com :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
