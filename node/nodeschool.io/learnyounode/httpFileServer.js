ae88ae76a6dbef95ed4ff9f653f575e167fddbbf
Peter Havens <peter.havens@gmail.com>
2014-08-10 10:05:31 -0600

    Add missing semicolon.

--- node/nodeschool.io/learnyounode/httpFileServer.js
+++ node/nodeschool.io/learnyounode/httpFileServer.js
@@ -7,7 +7,7 @@ var file = process.argv[3];
 var port = Number(process.argv[2]);
 
 var server = http.createServer(function (request, response) {
-  response.writeHead(200, { 'content-type': 'text/plain' })
+  response.writeHead(200, { 'content-type': 'text/plain' });
   fs.createReadStream(file).pipe(response);
 });
 
