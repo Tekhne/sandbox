93fa17e45f54eeb30352f4527f191352b4e85669
Peter Havens <Peter.Havens@pinnacol.com>
2014-08-12 15:51:34 -0600

    Add 'CONCAT' solution.

---
+++ node/nodeschool.io/stream-adventure/concat.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+var concat = require('concat-stream');
+
+process.stdin.pipe(concat(function (buffer) {
+    console.log(buffer.toString().split("").reverse().join(""));
+}));
