8b1ac7975f8f075eb1e32d6d95af0adc314ecd83
Peter Havens <peter.havens@gmail.com>
2014-08-24 09:53:29 -0600

    Add 'CRYPT' solution.

---
+++ node/nodeschool.io/stream-adventure/crypt.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+var crypto = require('crypto');
+var passphrase = process.argv[2];
+var decipher = crypto.createDecipher('aes256', passphrase);
+
+process.stdin.pipe(decipher).pipe(process.stdout);
